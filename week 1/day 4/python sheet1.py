3b0474f710736c7d57aae58897a9f879b87aa595
PrinceSaxena-coder <princes.26300649@kalvium.community>
2026-08-13 08:51:23 +0530

    day5ofcoding

---
+++ week 1/day 4/python sheet1.py
@@ -0,0 +1,30 @@
+name = ("Prince Saxena");
+age = 19;
+city = ("Firozabad");
+print(name)
+print(age)
+print(city)
+#variable: Its the data type which contains the value in it and can be modified
+price = 500
+x=y=z= "Python","is","fun";
+print(x)
+a= 10
+b= 20
+c= 30
+print(a+b+c)
+"""here the program creates the area of the rectangle by getting input in the form of length and breadth"""
+length = 13
+breadth = 14
+area = (length*breadth)
+print(area)
+r = 10
+e = 20
+r = r+e
+e = e-r
+r = r-e
+print(e,r)
+#putting on hold for a while
+p=q=r= 100
+print(p,q,r)
+p=q=r= 1,2,3
+print(p,q,r)
